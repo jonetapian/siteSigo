50f04ac511a5a5d44fe15959fb56a38e5acc5cf1
Leonardo Vasconcelos <leo.vasconcelos2000@gmail.com>
2020-02-01 14:38:50 -0300

    add rotas e menu

--- Sigo/src/app/app.module.ts
+++ Sigo/src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './paginas/login/login.component';
+import { CadastroComponent } from './paginas/cadastro/cadastro.component';
+import { CamisetasComponent } from './paginas/camisetas/camisetas.component';
+import { ShortsComponent } from './paginas/shorts/shorts.component';
+import { TenisComponent } from './paginas/tenis/tenis.component';
+import { ContatoComponent } from './paginas/contato/contato.component';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MenuComponent } from './paginas/menu/menu.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    CamisetasComponent,
+    ShortsComponent,
+    TenisComponent,
+    ContatoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Sigo/src/app/paginas/tenis/tenis.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TenisComponent } from './tenis.component';
+
+describe('TenisComponent', () => {
+  let component: TenisComponent;
+  let fixture: ComponentFixture<TenisComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TenisComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TenisComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
