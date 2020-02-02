563b5eb448a06429afeb75010ca855d4dd4dd255
Victor Diniz <victorhenriquediniz@gmail.com>
2020-02-01 21:16:23 -0300

    adding firebase to the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCd_dUFIZKXp1tRyFZCFcTMB0PLz5QTErs",
+    authDomain: "sigo-ffd8c.firebaseapp.com",
+    databaseURL: "https://sigo-ffd8c.firebaseio.com",
+    projectId: "sigo-ffd8c",
+    storageBucket: "sigo-ffd8c.appspot.com",
+    messagingSenderId: "747709319714",
+    appId: "1:747709319714:web:283ed535c492206bd2d87b",
+    measurementId: "G-JYWFWZL6SW"
+  }
 };
 
 /*
