d88979933ff5c0b9e3e512c59b7063d335164733
Leonardo Vasconcelos <leo.vasconcelos2000@gmail.com>
2020-02-02 13:53:44 -0300

    adicionado bootstrap e paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 import { environment } from '../environments/environment';
+import { LoginComponent } from './paginas/login/login.component';
+import { CadastroComponent } from './paginas/cadastro/cadastro.component';
+import { CamisetasComponent } from './paginas/camisetas/camisetas.component';
+import { ShortsComponent } from './paginas/shorts/shorts.component';
+import { TenisComponent } from './paginas/tenis/tenis.component';
+import { ContatoComponent } from './paginas/contato/contato.component';
+import { MenuComponent } from './paginas/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    CamisetasComponent,
+    ShortsComponent,
+    TenisComponent,
+    ContatoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
